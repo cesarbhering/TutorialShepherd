9442bddaa655918a38c71b4c75d694999615d8d0
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2022-08-07 14:08:29 -0300

    Primeiro Post

---
+++ pages/index.vue
@@ -0,0 +1,45 @@
+<template>
+<div>
+  <div id='shepherd-teste'>
+    <h1>TESTE TESTE </h1>
+  </div>
+
+</div>
+</template>
+<script>
+export default {
+  name: 'IndexPage',
+
+  mounted() {
+      this.$nextTick(() => {
+        const tour = this.$shepherd({
+          useModalOverlay: true,
+          defaultStepOptions: {
+            popperOptions: {
+              modifiers: [{ name: 'offset', options: { offset: [0, 50] } }]
+            }
+          }
+        });
+
+        tour.addStep({
+          attachTo: { element: '#shepherd-teste', on: 'top' },
+          text: 'Test'
+        });
+
+        tour.start();
+      });
+    }
+}
+</script>
+<style scoped>
+#shepherd-teste {
+  background-color: aqua;
+  margin: 25%;
+  width: 50%;
+  border: 3px solid green;
+  text-align: center;
+  padding: 10px;
+  height: 50%;
+  top: 5000px;
+}
+</style>
---
+++ plugins/shepherd.js
@@ -0,0 +1,4 @@
+import Vue from 'vue';
+import VueShepherd from 'vue-shepherd';
+import 'shepherd.js/dist/css/shepherd.css';
+Vue.use(VueShepherd);
